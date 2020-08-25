c63c2b01c6edd008c2cba8adbb367a6a446121fa
Guilherme Zanelato <guilherme.zanelato@4linux.com.br>
2020-08-24 22:24:28 -0300

    Primeiro algoritmo em python

---
+++ aula_01/ex_02.py
@@ -0,0 +1,11 @@
+# Escrever um script em python que seja uma saudação:
+
+## ele deverá capturar um nome informado pelo usuário, e a partir do nome
+## apresentar em tela a frase: Olá <nome infomado>, tudo bem com você? 
+
+#1: Pegar esse nome
+#2: armazenar o nome em algum lugar
+nome = input('Digite o seu nome: ')
+
+#3: apresentar em tela conjuntamente com a frase Olá <nome informado>, tudo bem com vc?
+print('Olá', nome, ', Tudo bem com você?')
