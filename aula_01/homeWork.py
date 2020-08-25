c7c87fdfb3fc63aa2740a13308aaa9331529891a
Guilherme Zanelato <guilherme.zanelato@4linux.com.br>
2020-08-24 22:24:49 -0300

    ADicionado o dever de casa

---
+++ aula_01/homeWork.py
@@ -0,0 +1,9 @@
+## Escrever um script em python que some dois números informado pelo usuário
+## e apresente o resultado da seguinte forma:
+## Números informados: 5 e 4
+## resultado esperado: 5 + 4 = 9
+
+### Realizar o Exercício do capítulo 1.
+
+
+
