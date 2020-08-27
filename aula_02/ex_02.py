18352fce13661332312c47f676011d3aa795313a
Guilherme Zanelato <guilherme.zanelato@4linux.com.br>
2020-08-26 21:04:02 -0300

    Adicionado exemplos de dados de coleção

---
+++ aula_02/ex_02.py
@@ -0,0 +1,47 @@
+### Coleções
+
+## Listas 
+
+## Mutável 
+lista = [0,1,2,4, 'String', 34.5, ]
+
+matriz = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]]
+
+## acessar o numero 5
+
+print(matriz[1][1]) # 5
+
+lista.append('Adiciona um elemento na lista')
+
+lista.pop() # Remove o último elemento da lista
+
+# help([])
+
+## Tuplas
+
+tupla = (0, 1, 2)
+
+## imutável
+
+## Dicionários
+
+dic = {
+        'nome':'Guilherme',
+        'idade': 32,
+        'cpf': '12345-123'
+
+        }
+
+print(dic['nome']) # 'Guilherme
+
+dic.values() # retorna todos os valores
+
+dic.keys() # retorna todas as chaves
+
+del dic['nome'] 
+
+# mutável 
+
