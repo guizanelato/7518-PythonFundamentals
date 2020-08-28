ee4da0b48f29bb634e3897d8c3a62bdbc9a7623f
Guilherme Zanelato <guilherme.zanelato@4linux.com.br>
2020-08-27 21:45:24 -0300

    adicionado exemplo de estrutura de um script em python

---
+++ aula_03/padrao_script.py
@@ -0,0 +1,49 @@
+##
+## imports
+## 
+
+###
+### Constantes
+### 
+
+PI = 3.14
+SELIC = 3.5
+
+###
+### funções 
+###
+
+def soma(n1, n2):
+    return n1 + n2
+
+def sub():
+    pass
+
+def mult():
+    pass
+
+def div():
+    pass
+
+
+###
+### funcao principal
+### definir o fluxo da aplicação 
+
+def main():
+    print(soma(10,12))
+
+###
+###
+###
+
+if __name__ == '__main__':
+    main()
+    
+
+
+
+
+
+
+
