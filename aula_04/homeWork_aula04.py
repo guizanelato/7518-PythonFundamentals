2d5f8a14584437617338858e34741485c01bcede
Guilherme Zanelato <guilherme.zanelato@4linux.com.br>
2020-08-28 21:59:55 -0300

    Adicionado conteúdo sobre módulos

---
+++ aula_04/homeWork_aula04.py
@@ -0,0 +1,16 @@
+"""
+1) criar um pacote de funcionalidade da lanchonete e separar o
+   arquivo principal de funções 'backend'
+
+
+"""
+
+
+"""
+2) Instalar os pacotes:
+
+    - pymongo
+    - pymysql
+    - psycopg2 
+
+"""
---
+++ aula_04/modulos.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import calc as jujuba
+
+#from calc.func import *
+
+
+print(jujuba.dif(1,2), jujuba.soma(1,2))
+
+exit()
+
+# Módulos
+## um arquivo que possui códigos na linguagem python, e eu posso fazer uso dele em 
+## qualquer script
+
+## Pacote
+## Um diretório que contém um ou mais módulos em python. Para criar um pacote
+## Eu preciso inicializar os módulos dentro do arquivo __init__
+
+
+## import modulo as apelido
+
+## A partir da biblioteca nativa
+## random, time, sys, os, re
+##
+import sys
+
+
+print(f'Este é um argumento de linha de comando {sys.argv[1]}')
+
+
+### instalar módulos através do pip - https://pypi.org/
+
+
+### criar módulos
+
